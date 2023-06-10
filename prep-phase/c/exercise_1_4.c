f6925afec4e7d050c7247e2d8e0c188ad5166def
Derek Huang <derekh919@gmail.com>
2023-06-09 21:20:55 -0400

    pre-commit

--- prep-phase/c/exercise_1_4.c
+++ prep-phase/c/exercise_1_4.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
+int main() {
+  float fahr, celsius;
+  int lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
 
-int main()
-{
-    float fahr, celsius;
-    int lower, upper, step;
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    celsius = lower;
-    printf("Celsius  Fahrenheit\n");
-    while (celsius <= upper) {
-        fahr = (celsius * (9 / 5)) + 32;
-        printf("%7.0f %11.0f\n", celsius, fahr);
-        celsius = celsius + step;
-    }
+  celsius = lower;
+  printf("Celsius  Fahrenheit\n");
+  while (celsius <= upper) {
+    fahr = (celsius * (9 / 5)) + 32;
+    printf("%7.0f %11.0f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
 }
