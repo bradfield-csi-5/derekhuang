07507e5a15553c645067d9bf5b5189a7ca1a7a85
Derek Huang <derekh919@gmail.com>
2023-06-11 14:05:32 -0400

    nice formatting

--- prep-phase/c/exercise_1_13.c
+++ prep-phase/c/exercise_1_13.c
@@ -5,25 +5,44 @@
 #define MAX_WORDS 10
 
 int main(int argc, char **argv) {
-  int c, longest = 0, printed = 0;
-  int word_len;
+  int c, word_len, longest = 0, printed = 0;
   int histogram[MAX_WORDS] = {0};
+  char *words[MAX_WORDS];
 
   for (int i = 1; i < argc; i++) {
     word_len = (int)strlen(argv[i]);
     longest = fmax(longest, word_len);
     histogram[i - 1] = word_len;
+    words[i - 1] = argv[i];
   }
+
+  printf("Histogram\n");
+  printf("-------------\n");
 
   while (longest > 0) {
     for (int i = 0; i < MAX_WORDS; i++) {
       if (histogram[i] >= longest) {
-        printf("#");
+        printf("# ");
       } else {
-        printf(" ");
+        printf("  ");
       }
     }
     printf("\n");
     longest--;
+  }
+
+  for (int i = 0; i < MAX_WORDS; i++) {
+    if (histogram[i] > 0) {
+      printf("%d ", i);
+    }
+  }
+  printf("\n\n");
+
+  printf("Words\n");
+  printf("-------------\n");
+  for (int i = 0; i < MAX_WORDS; i++) {
+    if (histogram[i] > 0) {
+      printf("%d: %s\n", i, words[i]);
+    }
   }
 }
