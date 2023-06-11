3371a87f586366b0ea14ea7f32e625b845737d07
Derek Huang <derekh919@gmail.com>
2023-06-11 14:35:33 -0400

    add -v flag to print vertically or horizontally

--- prep-phase/c/exercise_1_13.c
+++ prep-phase/c/exercise_1_13.c
@@ -1,42 +1,78 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_WORDS 10
 
 int main(int argc, char **argv) {
-  int c, word_len, longest = 0, printed = 0;
+  int c;
+  int word_len;
+  int arg_start = 1;
+  int arg_offset = 1;
+  int longest = 0;
+  int printed = 0;
+  int vertical = 0;
   int histogram[MAX_WORDS] = {0};
   char *words[MAX_WORDS];
 
-  for (int i = 1; i < argc; i++) {
-    word_len = (int)strlen(argv[i]);
+  while ((c = getopt(argc, argv, "v")) != -1) {
+    switch (c) {
+      case 'v':
+        vertical = 1;
+        break;
+      default:
+        abort();
+    }
+  }
+
+  if (vertical == 1) {
+    arg_start = 2;
+    arg_offset = 2;
+  }
+
+  for (; arg_start < argc; arg_start++) {
+    word_len = (int)strlen(argv[arg_start]);
     longest = fmax(longest, word_len);
-    histogram[i - 1] = word_len;
-    words[i - 1] = argv[i];
+    histogram[arg_start - arg_offset] = word_len;
+    words[arg_start - arg_offset] = argv[arg_start];
   }
 
   printf("Histogram\n");
   printf("-------------\n");
 
-  while (longest > 0) {
+  if (vertical == 1) {
+    while (longest > 0) {
+      for (int i = 0; i < MAX_WORDS; i++) {
+        if (histogram[i] >= longest) {
+          printf("# ");
+        } else {
+          printf("  ");
+        }
+      }
+      printf("\n");
+      longest--;
+    }
+
     for (int i = 0; i < MAX_WORDS; i++) {
-      if (histogram[i] >= longest) {
-        printf("# ");
-      } else {
-        printf("  ");
+      if (histogram[i] > 0) {
+        printf("%d ", i);
       }
     }
     printf("\n");
-    longest--;
-  }
-
-  for (int i = 0; i < MAX_WORDS; i++) {
-    if (histogram[i] > 0) {
-      printf("%d ", i);
+  } else {
+    for (int i = 0; i < MAX_WORDS; i++) {
+      if (histogram[i] > 0) {
+        printf("%d: ", i);
+        for (int j = 0; j < histogram[i]; j++) {
+          printf("# ");
+        }
+        printf("\n");
+      }
     }
   }
-  printf("\n\n");
+  printf("\n");
 
   printf("Words\n");
   printf("-------------\n");
