98488fb6af9fed8785fde95ecd3da827d9efe45a
Derek Huang <derekh919@gmail.com>
2023-06-28 22:33:35 -0400

    2-7

---
+++ prep-phase/c/book/exercise_2_7.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+
+/*
+Write a function `invert(x, p, n)` that returns `x` with the `n` bits that begin
+at position `p` inverted (i.e., 1 changed into 0 and vice versa), leaving the
+others unchanged.
+*/
+
+unsigned invert(unsigned x, unsigned p, unsigned n) {
+  unsigned x_mask = ((1 << n) - 1) << (p + 1 - n);
+  return x ^ x_mask;
+}
+
+int main(int argc, char **argv) {
+  unsigned x = 0b11110101;
+  assert(invert(x, 4, 2) == 0b11101101);
+  assert(invert(x, 5, 2) == 0b11000101);
+  assert(invert(x, 3, 4) == 0b11111010);
+  assert(invert(x, 7, 8) == 0b00001010);
+  printf("All Passed!\n");
+  return 0;
+}
