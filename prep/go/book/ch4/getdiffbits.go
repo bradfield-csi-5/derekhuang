96d8b3cfd3a25c9a0e32912506ae6c9baca8093e
Derek Huang <derekh919@gmail.com>
2023-07-06 18:42:53 -0400

    4.1

---
+++ prep/go/book/ch4/getdiffbits.go
@@ -0,0 +1,24 @@
+// exercise 4.1
+package getdiffbits
+
+import (
+	"crypto/sha256"
+)
+
+func popCount(x uint64) int {
+	var c int
+	for ; x > 0; c++ {
+		x &= (x - 1)
+	}
+	return c
+}
+
+func GetDiffBits(a string, b string) int {
+	var h1 = sha256.Sum256([]byte(a))
+	var h2 = sha256.Sum256([]byte(b))
+	var c int
+	for i := range h1 {
+		c += popCount(uint64(h1[i] ^ h2[i]))
+	}
+	return c
+}
---
+++ prep/go/book/ch4/getdiffbits_test.go
@@ -0,0 +1,25 @@
+package getdiffbits
+
+import (
+	"testing"
+)
+
+var tests = []struct {
+	s1  string
+	s2  string
+	out int
+}{
+	{"a", "a", 0},
+	{"a", "b", 126},
+	{"a", "c", 124},
+	{"a", "d", 137},
+}
+
+func TestGetDiffBits(t *testing.T) {
+	for _, tt := range tests {
+		r := GetDiffBits(tt.s1, tt.s2)
+		if r != tt.out {
+			t.Errorf("GetDiffBits(%s, %s) = <%d> want <%d>", tt.s1, tt.s2, r, tt.out)
+		}
+	}
+}
