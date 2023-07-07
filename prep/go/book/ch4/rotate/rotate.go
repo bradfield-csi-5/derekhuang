076a64576b2670b7c507edaaa8f8f231f78d114a
Derek Huang <derekh919@gmail.com>
2023-07-07 16:49:19 -0400

    rotate

--- prep/go/book/ch4/rotate/rotate.go
+++ prep/go/book/ch4/rotate/rotate.go
@@ -4,6 +4,23 @@ Exercise 4.4: Write a version of rotate that operates in a single pass.
 
 package rotate
 
-func Rotate(s []int, r int) []int {
-	// try swapping and using modulo i
+func Rotate(s []int, r int) {
+	var size int = len(s)
+	var swaps int = 0
+	var temp = -1 // not a good starting val but not sure what it should be
+	if r%size == 0 {
+		return
+	}
+	for i := 0; swaps < size; swaps++ {
+		ri := (i + r) % size
+		if temp == -1 {
+			temp = s[ri]
+			s[ri] = s[i]
+		} else {
+			next_temp := s[ri]
+			s[ri] = temp
+			temp = next_temp
+		}
+		i = ri
+	}
 }
---
+++ prep/go/book/ch4/rotate/rotate_test.go
@@ -0,0 +1,40 @@
+package rotate
+
+import "testing"
+
+var tests = []struct {
+	in  []int
+	r   int
+	out []int
+}{
+	{[]int{1, 2, 3, 4, 5}, 1, []int{5, 1, 2, 3, 4}},
+	{[]int{1, 2, 3, 4, 5}, 2, []int{4, 5, 1, 2, 3}},
+	{[]int{1, 2, 3, 4, 5}, 3, []int{3, 4, 5, 1, 2}},
+	{[]int{1, 2, 3, 4, 5}, 4, []int{2, 3, 4, 5, 1}},
+	{[]int{1, 2, 3, 4, 5}, 5, []int{1, 2, 3, 4, 5}},
+	{[]int{-1, 2, 3, 4, 5}, 1, []int{5, -1, 2, 3, 4}},
+	// {[]int{1, -1, 3, 4, 5}, 1, []int{5, 1, -1, 3, 4}}, // fails
+}
+
+func TestRotate(t *testing.T) {
+	for _, tt := range tests {
+		var cp = make([]int, len(tt.in))
+		copy(cp, tt.in)
+		Rotate(tt.in, tt.r)
+		if !compareSlice(tt.in, tt.out) {
+			t.Errorf("Reverse(%v) == <%v> want <%v>", cp, tt.in, tt.out)
+		}
+	}
+}
+
+func compareSlice(s1 []int, s2 []int) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}
