2dcec86806731afc49abbca6ba22734d5fcb7058
Derek Huang <derekh919@gmail.com>
2023-07-15 15:23:12 -0400

    test intersection and union

--- prep/go/xkcd/utils_test.go
+++ prep/go/xkcd/utils_test.go
@@ -29,18 +29,86 @@ var normalizeTests = []struct {
 func TestNormalize(t *testing.T) {
 	for _, tt := range normalizeTests {
 		r := normalize(tt.in...)
-		if !equal(r, tt.out) {
+		if !compStrSet(r, tt.out) {
 			t.Errorf("normalize(%v) == <%v> want <%v>", tt.in, r, tt.out)
 		}
 	}
 }
 
-func equal(s1 StrSet, s2 StrSet) bool {
+func compStrSet(s1, s2 StrSet) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
 	for word := range s1 {
 		if _, exists := s2[word]; !exists {
+			return false
+		}
+	}
+	return true
+}
+
+var intersectionTests = []struct {
+	s1  IntSet
+	s2  IntSet
+	out IntSet
+}{
+	{s1: IntSet{1: true, 2: true, 3: true}, s2: IntSet{}, out: IntSet{}},
+	{s1: IntSet{}, s2: IntSet{1: true, 2: true, 3: true}, out: IntSet{}},
+	{
+		s1:  IntSet{1: true, 2: true, 3: true, 4: true, 5: true},
+		s2:  IntSet{1: true, 2: true, 3: true},
+		out: IntSet{1: true, 2: true, 3: true},
+	},
+	{
+		s1:  IntSet{1: true, 2: true, 3: true},
+		s2:  IntSet{1: true, 2: true, 3: true, 4: true, 5: true},
+		out: IntSet{1: true, 2: true, 3: true},
+	},
+}
+
+func TestSetIntersection(t *testing.T) {
+	for _, tt := range intersectionTests {
+		tt.s1.Intersection(tt.s2)
+		if !compIntSet(tt.s1, tt.out) {
+			t.Errorf("s1.Intersection(s2) == <%v> want <%v>", tt.s1, tt.out)
+		}
+	}
+}
+
+var unionTests = []struct {
+	s1  IntSet
+	s2  IntSet
+	out IntSet
+}{
+	{s1: IntSet{1: true, 2: true, 3: true}, s2: IntSet{}, out: IntSet{1: true, 2: true, 3: true}},
+	{s1: IntSet{}, s2: IntSet{1: true, 2: true, 3: true}, out: IntSet{1: true, 2: true, 3: true}},
+	{
+		s1:  IntSet{1: true, 2: true, 3: true, 4: true, 5: true},
+		s2:  IntSet{1: true, 2: true, 3: true},
+		out: IntSet{1: true, 2: true, 3: true, 4: true, 5: true},
+	},
+	{
+		s1:  IntSet{1: true, 2: true, 3: true},
+		s2:  IntSet{1: true, 2: true, 3: true, 4: true, 5: true},
+		out: IntSet{1: true, 2: true, 3: true, 4: true, 5: true},
+	},
+}
+
+func TestSetUnion(t *testing.T) {
+	for _, tt := range unionTests {
+		tt.s1.Union(tt.s2)
+		if !compIntSet(tt.s1, tt.out) {
+			t.Errorf("s1.Union(s2) == <%v> want <%v>", tt.s1, tt.out)
+		}
+	}
+}
+
+func compIntSet(s1, s2 IntSet) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if _, exists := s2[i]; !exists {
 			return false
 		}
 	}
