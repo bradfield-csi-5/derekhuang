2ca13a3e274ab4058105e1049925f4f7bb0b6abc
Derek Huang <derekh919@gmail.com>
2023-08-14 23:04:40 -0400

    decr sum_to_n indents

--- 01_intro_to_systems/assembly-prework/sum_to_n.asm
+++ 01_intro_to_systems/assembly-prework/sum_to_n.asm
@@ -1,13 +1,13 @@
 section .text
 global sum_to_n
 sum_to_n:
-        mov     rax, 0          ; init result to 0
-        jmp     .test
+    mov     rax, 0          ; init result to 0
+    jmp     .test
 
 .loop:
-        add     rax, rdi
-        dec     rdi
+    add     rax, rdi
+    dec     rdi
 
 .test:
-        cmp     rdi, 0          ; n > 0?
-        jg      .loop           ; keep looping
+    cmp     rdi, 0          ; n > 0?
+    jg      .loop           ; keep looping
