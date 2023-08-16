fc04ffa124566a12a027cb3bbba1c94d577b9476
Derek Huang <derekh919@gmail.com>
2023-08-15 22:15:55 -0400

    binary convert tests passing

--- 01_intro_to_systems/assembly-prework/binary_convert.asm
+++ 01_intro_to_systems/assembly-prework/binary_convert.asm
@@ -1,4 +1,19 @@
 section .text
 global binary_convert
 binary_convert:
-	ret
+    xor     rax, rax
+    mov     rcx, 2
+
+next:
+    cmp     byte [rdi], 0
+    je      return
+    mov     bl, [rdi]
+    sub     bl, 48
+    mul     rcx
+    add     al, bl
+    inc     rdi
+    jmp     next
+
+return:
+    ret
+
