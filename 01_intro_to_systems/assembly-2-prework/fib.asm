bf5084944660656bb53b4edc1e3b480cb9f42397
Derek Huang <derekh919@gmail.com>
2023-08-17 22:18:27 -0400

    fib

--- 01_intro_to_systems/assembly-2-prework/fib.asm
+++ 01_intro_to_systems/assembly-2-prework/fib.asm
@@ -1,4 +1,19 @@
 section .text
 global fib
 fib:
+	cmp rdi, 1
+	jg .rec
+	mov eax, edi	; if n <= 1 return
+	ret
+
+.rec:
+	push rdi		; save n
+	dec rdi			; n - 1
+	call fib
+	pop rdi			; get n for next recursive call
+	push rax		; save retval
+	sub rdi, 2		; n - 2
+	call fib
+	pop rdi			; get retval and put in rdi
+	add eax, edi	; add the results of recursive calls
 	ret
