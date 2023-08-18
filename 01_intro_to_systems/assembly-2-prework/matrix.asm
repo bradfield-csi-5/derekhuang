ae13499cea2c74adea1e04e9552c315a8649df2b
Derek Huang <derekh919@gmail.com>
2023-08-18 00:06:16 -0400

    matrix

--- 01_intro_to_systems/assembly-2-prework/matrix.asm
+++ 01_intro_to_systems/assembly-2-prework/matrix.asm
@@ -1,9 +1,13 @@
 section .text
 global index
 index:
-	; rdi: matrix
-	; rsi: rows
-	; rdx: cols
-	; rcx: rindex
-	; r8: cindex
-	ret
+    ; rdi: matrix
+    ; rsi: rows
+    ; rdx: cols
+    ; rcx: rindex
+    ; r8: cindex
+    imul rcx, rdx
+    lea rax, [rcx * 4]
+    lea rax, [rax + r8 * 4]
+    mov eax, [rdi + rax]
+    ret
