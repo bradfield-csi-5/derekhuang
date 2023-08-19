ef9e9e47f688259d5673b75a761c00dd61f37520
Derek Huang <derekh919@gmail.com>
2023-08-18 22:25:44 -0400

    cone

--- 01_intro_to_systems/assembly-2-prework/cone.asm
+++ 01_intro_to_systems/assembly-2-prework/cone.asm
@@ -3,4 +3,16 @@ default rel
 section .text
 global volume
 volume:
+    ; v = pi * r^2 * h/3
+    ; radius xmm0
+    ; height xmm1
+    mulss       xmm0, xmm0      ; r^2
+    mov         rsi, 3          ; prepare 3 for division
+    vcvtsi2ss   xmm2, rsi       ; convert int 3 to single precision floating point
+    divss       xmm1, xmm2      ; h/3
+    mulss       xmm0, xmm1      ; r^2 * h/3
+    mulss       xmm0, [pi]
  	ret
+
+section .data
+pi: dd 3.14159265359
