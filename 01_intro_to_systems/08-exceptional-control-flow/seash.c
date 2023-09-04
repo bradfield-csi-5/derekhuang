5d6144c8aa583049e287d3608772b0065410dce6
Derek Huang <derekh919@gmail.com>
2023-09-04 16:15:37 -0400

    use bools

--- 01_intro_to_systems/08-exceptional-control-flow/seash.c
+++ 01_intro_to_systems/08-exceptional-control-flow/seash.c
@@ -1,27 +1,29 @@
 #include "csapp.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #define SHELL "\U0001F41A"
 #define WAVE "\U0001F30A"
 
-void eval(char *cmdline, short repl) {
+void eval(char *cmdline, bool repl) {
   if (repl) {
     fputs(cmdline, stdout);
     exit(0);
+  } else {
+    fputs(cmdline, stdout);
   }
-  fputs(cmdline, stdout);
 }
 
 int main(int argc, char **argv) {
   char cmdline[MAXLINE];
   int opt;
 
-  char cmd_mode = 0;
+  bool cmd_mode = false;
   while ((opt = getopt(argc, argv, "c")) != -1) {
     switch (opt) {
     case 'c':
-      cmd_mode = 1;
+      cmd_mode = true;
       break;
     }
   }
@@ -30,7 +32,7 @@ int main(int argc, char **argv) {
     printf("%s ", SHELL);
 
     if (cmd_mode) {
-      eval(argv[optind], 1);
+      eval(argv[optind], true);
     } else {
       Fgets(cmdline, MAXLINE, stdin);
 
@@ -40,7 +42,7 @@ int main(int argc, char **argv) {
         exit(0);
       }
 
-      eval(cmdline, 0);
+      eval(cmdline, false);
     }
   }
 }
