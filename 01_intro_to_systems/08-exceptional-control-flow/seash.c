0a1b031820f833b299f9665b7510ed1e567ae97d
Derek Huang <derekh919@gmail.com>
2023-09-04 16:10:13 -0400

    cmd mode

--- 01_intro_to_systems/08-exceptional-control-flow/seash.c
+++ 01_intro_to_systems/08-exceptional-control-flow/seash.c
@@ -1,19 +1,46 @@
 #include "csapp.h"
 #include <stdio.h>
+#include <unistd.h>
 
-void eval(char *cmdline) { fputs(cmdline, stdout); }
+#define SHELL "\U0001F41A"
+#define WAVE "\U0001F30A"
+
+void eval(char *cmdline, short repl) {
+  if (repl) {
+    fputs(cmdline, stdout);
+    exit(0);
+  }
+  fputs(cmdline, stdout);
+}
 
 int main(int argc, char **argv) {
   char cmdline[MAXLINE];
+  int opt;
+
+  char cmd_mode = 0;
+  while ((opt = getopt(argc, argv, "c")) != -1) {
+    switch (opt) {
+    case 'c':
+      cmd_mode = 1;
+      break;
+    }
+  }
 
   while (1) {
-    printf("\U0001F41A ");
+    printf("%s ", SHELL);
 
-    Fgets(cmdline, MAXLINE, stdin);
+    if (cmd_mode) {
+      eval(argv[optind], 1);
+    } else {
+      Fgets(cmdline, MAXLINE, stdin);
 
-    if (feof(stdin))
-      exit(0);
+      if (feof(stdin)) {
+        printf("%s %s %s Sea you later %s %s %s\n", WAVE, WAVE, WAVE, WAVE,
+               WAVE, WAVE);
+        exit(0);
+      }
 
-    eval(cmdline);
+      eval(cmdline, 0);
+    }
   }
 }
