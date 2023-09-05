fb7869c473c22ede1bdff5a32a6cf5d9af5f73eb
Derek Huang <derekh919@gmail.com>
2023-09-04 22:05:01 -0400

    pangram.asm

--- 01_intro_to_systems/03-assembly/pangram.asm
+++ 01_intro_to_systems/03-assembly/pangram.asm
@@ -6,30 +6,13 @@ pangram:
 .loop:
     movzx edx, byte [rdi]   ; get char
     cmp edx, 0              ; check for null
-    je .return
-    cmp edx, 'a'            ; check if uppercase
-    jl .check_upper
-    cmp edx, 'z'            ; check if letter
-    jg .invalid
-    sub edx, 'a'            ; lowercase letter; normalize
+    je .return              ; short circuit
+    or edx, 32              ; normalize to lowercase
+    sub edx, 'a'            ; normalize to an index between 0 and 25
     bts ecx, edx            ; set corresponding letter bit
     inc rdi
     jmp .loop
 
-.check_upper:
-    cmp edx, 'A'            ; check if letter
-    jl .invalid
-    cmp edx, 'Z'
-    jg .invalid
-    sub edx, 'A'            ; uppercase letter; normalize
-    bts ecx, edx            ; set corresponding letter bit
-    inc rdi
-    jmp .loop
-
-.invalid:
-    inc rdi                 ; skip char and keep looping
-    jmp .loop
- 
 .return:
     xor rax, rax
     and ecx, 0x03ffffff     ; check for missing bits in the first 26
