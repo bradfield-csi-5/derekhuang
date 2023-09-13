fdab47ed3572cc645f605f024ac3b1ba2e6c7d20
Derek Huang <derekh919@gmail.com>
2023-09-12 23:56:27 -0400

    refactor to pull in ethernet header parts

--- 02-computer-networks/01-layers-of-protocols/parse_cap.c
+++ 02-computer-networks/01-layers-of-protocols/parse_cap.c
@@ -9,10 +9,16 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MAXLINE 1024
+#define PACKLEN_SIZE 4
+#define FULL_PACKLEN_SIZE 4
+#define MAC_DEST_SIZE 6
+#define MAC_SRC_SIZE 6
+#define ETHERTYPE_SIZE 2
 
-void print_byte(unsigned char *buf, bool big_endian, bool decimal) {
+void print_byte(unsigned char buf[4], bool big_endian, bool decimal) {
   if (big_endian) {
     for (int i = 0; i < 4; i++) {
       if (buf[i] == 0) {
@@ -48,8 +54,17 @@ void print_byte(unsigned char *buf, bool big_endian, bool decimal) {
   }
 }
 
+unsigned int cbtoi(unsigned char buf[4]) {
+  int left_shift = 0;
+  int ret = 0;
+  for (int i = 0; i < 4; i++) {
+    ret |= buf[i] << left_shift;
+    left_shift += 8;
+  }
+  return ret;
+}
+
 int main(int argc, char **argv) {
-  int buf[MAXLINE];
   FILE *fp;
 
   if ((fp = fopen("net.cap", "r")) == NULL) {
@@ -57,23 +72,47 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char buf[MAXLINE];
+
+  unsigned char packlen_buf[PACKLEN_SIZE];
+  unsigned int packlen;
+
+  unsigned char full_packlen_buf[FULL_PACKLEN_SIZE];
+  unsigned int full_packlen;
+
+  char mac_dest[MAC_DEST_SIZE];
+  char mac_src[MAC_SRC_SIZE];
+
+  char ethertype[ETHERTYPE_SIZE];
+
+  int nitems = 22;
   int count = 0;
-  int nitems = 2;
-  int pack_len;
-  int full_pack_len;
 
   // Packet length starts at byte 33
   fseek(fp, 32, SEEK_CUR);
-  while (fread(buf, sizeof(int), nitems, fp)) {
-    pack_len = buf[0];
-    full_pack_len = buf[1];
+  while (fread(buf, sizeof(char), nitems, fp)) {
+    // TODO: there has to be a better way...
+    memcpy(packlen_buf, buf, PACKLEN_SIZE);
+    memcpy(full_packlen_buf, buf + PACKLEN_SIZE, FULL_PACKLEN_SIZE);
+    memcpy(mac_dest, buf + PACKLEN_SIZE + FULL_PACKLEN_SIZE, MAC_DEST_SIZE);
+    memcpy(mac_src, buf + PACKLEN_SIZE + FULL_PACKLEN_SIZE + MAC_DEST_SIZE,
+           MAC_SRC_SIZE);
+    memcpy(ethertype,
+           buf + PACKLEN_SIZE + FULL_PACKLEN_SIZE + MAC_DEST_SIZE +
+               MAC_SRC_SIZE,
+           ETHERTYPE_SIZE);
 
-    if (pack_len != full_pack_len) {
-      printf("Partial packet: %d of %d\n", pack_len, full_pack_len);
+    packlen = cbtoi(packlen_buf);
+    full_packlen = cbtoi(full_packlen_buf);
+
+    if (packlen != full_packlen) {
+      printf("Partial packet: %d of %d\n", packlen, full_packlen);
     }
 
-    // Jump to the next packet length byte in the next header
-    if (fseek(fp, pack_len + 8, SEEK_CUR) != 0) {
+    // Jump to the next packet length byte in the next header.
+    // Subtract 6 from packlen to offset the additional bytes being loaded into
+    // the buffer for the parts of the ethernet header we care about.
+    if (fseek(fp, packlen - 6, SEEK_CUR) != 0) {
       perror("Error seeking");
       return 1;
     }
