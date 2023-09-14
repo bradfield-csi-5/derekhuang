a1f51b2c5eb79f4f4d2aa738568934e294c414c1
Derek Huang <derekh919@gmail.com>
2023-09-13 23:32:37 -0400

    nicer prints

--- 02-computer-networks/01-layers-of-protocols/parse_cap.c
+++ 02-computer-networks/01-layers-of-protocols/parse_cap.c
@@ -23,11 +23,15 @@
 #include <string.h>
 
 #define FILE_HEADER_LEN 24
+
 #define PACKLEN_SIZE 4
 #define FULL_PACKLEN_SIZE 4
+
 #define MAC_DEST_SIZE 6
 #define MAC_SRC_SIZE 6
 #define ETHERTYPE_SIZE 2
+#define IPV4 0x8
+#define IPV6 0xdd86
 
 unsigned int ctoi(unsigned char *buf, int size) {
   int shift = 0;
@@ -75,29 +79,39 @@ int main(int argc, char **argv) {
   fread(buf, filelen, 1, fp);
 
   int count = 0;
-  unsigned int packlen = 0;
-  unsigned int full_packlen = 0;
+
+  unsigned int packlen;
+  unsigned int full_packlen;
+
+  unsigned int ethertype;
+  char *ethertype_str;
+
+  /* unsigned char ip_ver; */
+  /* unsigned char ip_header_len; */
 
   // Skip the per-file header
   buf += FILE_HEADER_LEN;
 
   while (*buf) {
+    count++;
+
     // Skip the packet timestamp bytes
     buf += 8;
 
+    printf("========== Packet %d ==========\n", count);
     // Read the packet length and move the buffer forward
     packlen = ctoi(buf, PACKLEN_SIZE);
+    printf("Captured length: %d\n", packlen);
     buf += PACKLEN_SIZE;
 
     // Read the un-truncated packet length and move the buffer forward
     full_packlen = ctoi(buf, FULL_PACKLEN_SIZE);
+    printf("Untruncated length: %d\n", full_packlen);
     // This is important to jump to the start of the next packet
     buf += FULL_PACKLEN_SIZE;
+    printf("\n");
 
-    if (packlen != full_packlen) {
-      printf("Partial packet captured: %d of %d\n", packlen, full_packlen);
-    }
-
+    printf("========== Ethernet Headers ==========\n");
     // Print MAC addresses, move buffer forward, and adjust packlen
     printf("MAC destination: ");
     print_mac_addr(buf, MAC_DEST_SIZE);
@@ -105,16 +119,36 @@ int main(int argc, char **argv) {
     packlen -= MAC_DEST_SIZE;
     printf("\n");
 
-    printf("MAC destination: ");
+    printf("MAC source: ");
     print_mac_addr(buf, MAC_SRC_SIZE);
     buf += MAC_SRC_SIZE;
     packlen -= MAC_SRC_SIZE;
     printf("\n");
+
+    ethertype = ctoi(buf, ETHERTYPE_SIZE);
+    if (ethertype == IPV4) {
+      ethertype_str = "IPv4";
+    } else if (ethertype == IPV6) {
+      ethertype_str = "IPv6";
+    } else {
+      perror("Error parsing ethertype");
+      return 1;
+    }
+    printf("Ethertype: %s\n", ethertype_str);
+    buf += ETHERTYPE_SIZE;
+    packlen -= ETHERTYPE_SIZE;
+    printf("\n");
+
+    printf("========== IP Headers ==========\n");
+    printf("IP version: %d\n", *buf >> 4);
+    printf("IP header length: %d\n", *buf & 0xf0);
+    buf += 1;
+    packlen -= 1;
     printf("\n");
 
     buf += packlen;
 
-    count++;
+    printf("\n");
   }
 
   fclose(fp);
