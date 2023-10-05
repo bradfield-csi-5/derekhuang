05e90d20bde4c03f061907b8b7f02645ef1485a4
Derek Huang <derekh919@gmail.com>
2023-10-04 20:32:18 -0400

    receiver cleanup

--- 02-computer-networks/04-reliable-data-delivery/receiver/receiver.go
+++ 02-computer-networks/04-reliable-data-delivery/receiver/receiver.go
@@ -20,14 +20,8 @@ func main() {
 	fmt.Printf("Receiver listening on port %d\n", port)
 	for {
 		buf := make([]byte, 4)
-		n, _, err := unix.Recvfrom(fd, buf, 0)
+		_, _, err := unix.Recvfrom(fd, buf, 0)
 		check(err)
-		if n == 0 {
-			fmt.Printf("No bytes received. Connection Closed.\n")
-		} else {
-			fmt.Printf("Received %d bytes from client\n", n)
-		}
-
 		fmt.Printf("Received: %v %s\n", buf, buf)
 	}
 }
