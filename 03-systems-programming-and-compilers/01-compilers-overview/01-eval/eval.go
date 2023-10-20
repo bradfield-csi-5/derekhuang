d357ce1989789bc32034aabaa7be163cb239663f
Derek Huang <derekh919@gmail.com>
2023-10-19 21:28:44 -0400

    working Evaluate

--- 03-systems-programming-and-compilers/01-compilers-overview/01-eval/eval.go
+++ 03-systems-programming-and-compilers/01-compilers-overview/01-eval/eval.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"errors"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
+	"strconv"
 )
 
 // Given an expression containing only int types, evaluate
-// the expression and return the result.
+// the expressiron and return the result.
 func Evaluate(expr ast.Expr) (int, error) {
-	// TODO
-	return 0, nil
+	switch v := expr.(type) {
+	case *ast.BasicLit:
+		switch v.Kind {
+		case token.INT:
+			i, err := strconv.Atoi(v.Value)
+			check(err)
+			return i, nil
+		}
+	case *ast.BinaryExpr:
+		x, err := Evaluate(v.X)
+		check(err)
+		y, err := Evaluate(v.Y)
+		check(err)
+		switch v.Op {
+		case token.ADD:
+			return x + y, nil
+		case token.SUB:
+			return x - y, nil
+		case token.MUL:
+			return x * y, nil
+		case token.QUO:
+			return x / y, nil
+		}
+	case *ast.ParenExpr:
+		return Evaluate(v.X)
+	}
+	return 0, errors.New("Missing case for ast.Expr")
 }
 
 func main() {
@@ -19,9 +46,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fset := token.NewFileSet()
-	err = ast.Print(fset, expr)
+	// fset := token.NewFileSet()
+	// err = ast.Print(fset, expr)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	res, err := Evaluate(expr)
 	if err != nil {
 		log.Fatal(err)
+	}
+	log.Printf("Eval: %d\n", res)
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
 	}
 }
--- 03-systems-programming-and-compilers/01-compilers-overview/01-eval/go.mod
+++ 03-systems-programming-and-compilers/01-compilers-overview/01-eval/go.mod
@@ -1,3 +1,3 @@
 module eval
 
-go 1.19
+go 1.21.3
